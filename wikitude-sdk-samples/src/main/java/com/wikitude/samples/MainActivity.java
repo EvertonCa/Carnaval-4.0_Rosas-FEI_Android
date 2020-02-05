5c7b5de2b5b5f676e257919698d39bb959f71666
EvertonCa <eve.023@hotmail.com>
2020-02-05 01:30:01 -0300

    All animations and transitions ok

--- wikitude-sdk-samples/src/main/java/com/wikitude/samples/MainActivity.java
+++ wikitude-sdk-samples/src/main/java/com/wikitude/samples/MainActivity.java
@@ -1,29 +1,15 @@
 package com.wikitude.samples;
 
-import com.wikitude.architect.ArchitectView;
-import com.wikitude.common.CallStatus;
-import com.wikitude.common.permission.PermissionManager;
-import com.wikitude.samples.util.PermissionUtil;
-import com.wikitude.samples.util.SampleCategory;
-import com.wikitude.samples.util.SampleData;
-import com.wikitude.samples.util.SampleJsonParser;
 import com.wikitude.sdksamples.R;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
-import android.widget.Toast;
-
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * The MainActivity is used to display the list of samples and handles the runtime
@@ -31,10 +17,6 @@ import java.util.List;
  */
 public class MainActivity extends AppCompatActivity {
 
-    private static final String sampleDefinitionsPath = "samples/samples.json";
-
-    private final PermissionManager permissionManager = ArchitectView.getPermissionManager();
-    private List<SampleCategory> categories;
     private ImageView splashImageView;
 
     @Override
@@ -43,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
         splashImageView = findViewById(R.id.splashImageView);
 
+        overridePendingTransition(0, 0);
+
         this.getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -50,20 +34,6 @@ public class MainActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-
-        final String json = SampleJsonParser.loadStringFromAssets(this, sampleDefinitionsPath);
-        categories = SampleJsonParser.getCategoriesFromJsonString(json);
-
-        for (final SampleCategory category : categories) {
-            for (final SampleData data : category.getSamples()) {
-                CallStatus status = ArchitectView.isDeviceSupporting(this, data.getArFeatures());
-                if (status.isSuccess()) {
-                    data.isDeviceSupporting(true, "");
-                } else {
-                    data.isDeviceSupporting(false, status.getError().getMessage());
-                }
-            }
-        }
 
         Handler handle = new Handler();
         handle.postDelayed(new Runnable() {
@@ -80,17 +50,21 @@ public class MainActivity extends AppCompatActivity {
         // first 0f, 1f mean scaling from Y-axis to Y-axis, meaning scaling from 0-100%
         // The two 0.5f mean animation will start from 50% of X-axis & 50% of Y-axis, i.e. from center
 
-        final ScaleAnimation fade_out =  new ScaleAnimation(1f, 10f, 1f, 10f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-        fade_out.setDuration(700);     // animation duration in milliseconds
-        fade_out.setFillAfter(true);    // If fillAfter is true, the transformation that this animation performed will persist when it is finished.
+        final ScaleAnimation scale_up =  new ScaleAnimation(0.5f, 3.5f, 0.5f, 3.5f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        scale_up.setDuration(700);     // animation duration in milliseconds
+        scale_up.setFillAfter(true);    // If fillAfter is true, the transformation that this animation performed will persist when it is finished.
 
-        final ScaleAnimation fade_in =  new ScaleAnimation(1f, 0.5f, 1f, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-        fade_in.setDuration(400);     // animation duration in milliseconds
-        fade_in.setFillAfter(true);    // If fillAfter is true, the transformation that this animation performed will persist when it is finished.
+        final ScaleAnimation scale_down =  new ScaleAnimation(1f, 0.5f, 1f, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        scale_down.setDuration(400);     // animation duration in milliseconds
+        scale_down.setFillAfter(true);    // If fillAfter is true, the transformation that this animation performed will persist when it is finished.
 
-        splashImageView.startAnimation(fade_in);
+        final ScaleAnimation scale_down_again =  new ScaleAnimation(3.5f, 0.0f, 3.5f, 0.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        scale_down_again.setDuration(400);     // animation duration in milliseconds
+        scale_down_again.setFillAfter(true);    // If fillAfter is true, the transformation that this animation performed will persist when it is finished.
 
-        fade_in.setAnimationListener(new Animation.AnimationListener() {
+        splashImageView.startAnimation(scale_down);
+
+        scale_down.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
                 ;
@@ -98,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                splashImageView.startAnimation(fade_out);
+                splashImageView.startAnimation(scale_up);
             }
 
             @Override
@@ -107,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        fade_out.setAnimationListener(new Animation.AnimationListener() {
+        scale_up.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
                 ;
@@ -115,7 +89,24 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                startAR();
+                splashImageView.startAnimation(scale_down_again);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+                ;
+            }
+        });
+
+        scale_down_again.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                ;
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                callMainActivity();
             }
 
             @Override
@@ -125,58 +116,11 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public boolean startAR()
+    public void callMainActivity()
     {
-        final SampleData sampleData = categories.get(0).getSamples().get(0);
-        final String[] permissions = PermissionUtil.getPermissionsForArFeatures(sampleData.getArFeatures());
-
-        if(!sampleData.getIsDeviceSupporting()) {
-            showDeviceMissingFeatures(sampleData.getIsDeviceSupportingError());
-        } else {
-            permissionManager.checkPermissions(MainActivity.this, permissions, PermissionManager.WIKITUDE_PERMISSION_REQUEST, new PermissionManager.PermissionManagerCallback() {
-                @Override
-                public void permissionsGranted(int requestCode) {
-                    final Intent intent = new Intent(MainActivity.this, sampleData.getActivityClass());
-                    intent.putExtra(SimpleArActivity.INTENT_EXTRAS_KEY_SAMPLE, sampleData);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(intent);
-                }
-
-                @Override
-                public void permissionsDenied(@NonNull String[] deniedPermissions) {
-                    Toast.makeText(MainActivity.this, getString(R.string.permissions_denied) + Arrays.toString(deniedPermissions), Toast.LENGTH_SHORT).show();
-                }
-
-                @Override
-                public void showPermissionRationale(final int requestCode, @NonNull String[] strings) {
-                    final AlertDialog.Builder alertBuilder = new AlertDialog.Builder(MainActivity.this);
-                    alertBuilder.setCancelable(true);
-                    alertBuilder.setTitle(R.string.permission_rationale_title);
-                    alertBuilder.setMessage(getString(R.string.permission_rationale_text) + Arrays.toString(permissions));
-                    alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            permissionManager.positiveRationaleResult(requestCode, permissions);
-                        }
-                    });
-
-                    AlertDialog alert = alertBuilder.create();
-                    alert.show();
-                }
-            });
-        }
-        return false;
+        Intent i = new Intent(MainActivity.this, StartActivity.class);
+        MainActivity.this.startActivity(i);
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        ArchitectView.getPermissionManager().onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
 
-    public void showDeviceMissingFeatures(String errorMessage) {
-        new AlertDialog.Builder(MainActivity.this)
-            .setTitle(R.string.device_missing_features)
-            .setMessage(errorMessage)
-            .show();
-    }
 }
---
+++ wikitude-sdk-samples/src/main/java/com/wikitude/samples/StartActivity.java
@@ -0,0 +1,135 @@
+package com.wikitude.samples;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.wikitude.architect.ArchitectView;
+import com.wikitude.common.CallStatus;
+import com.wikitude.common.permission.PermissionManager;
+import com.wikitude.samples.util.PermissionUtil;
+import com.wikitude.samples.util.SampleCategory;
+import com.wikitude.samples.util.SampleData;
+import com.wikitude.samples.util.SampleJsonParser;
+import com.wikitude.sdksamples.R;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StartActivity extends AppCompatActivity {
+
+    private static final String sampleDefinitionsPath = "samples/samples.json";
+
+    private final PermissionManager permissionManager = ArchitectView.getPermissionManager();
+    private List<SampleCategory> categories;
+
+    private ImageView buttonStart;
+    private ImageView rosasLogoView;
+    private ImageView feiLogoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        overridePendingTransition(0, 0);
+
+        setContentView(R.layout.activity_main);
+        buttonStart = findViewById(R.id.buttonStart);
+        rosasLogoView = findViewById(R.id.rosasLogoView);
+        feiLogoView = findViewById(R.id.feiLogoView);
+
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        Animation fadeIn = new AlphaAnimation(0, 1);
+        fadeIn.setInterpolator(new AccelerateInterpolator());
+        fadeIn.setStartOffset(200);
+        fadeIn.setDuration(1000);
+
+        buttonStart.startAnimation(fadeIn);
+        rosasLogoView.startAnimation(fadeIn);
+        feiLogoView.startAnimation(fadeIn);
+
+        final String json = SampleJsonParser.loadStringFromAssets(this, sampleDefinitionsPath);
+        categories = SampleJsonParser.getCategoriesFromJsonString(json);
+
+        for (final SampleCategory category : categories) {
+            for (final SampleData data : category.getSamples()) {
+                CallStatus status = ArchitectView.isDeviceSupporting(this, data.getArFeatures());
+                if (status.isSuccess()) {
+                    data.isDeviceSupporting(true, "");
+                } else {
+                    data.isDeviceSupporting(false, status.getError().getMessage());
+                }
+            }
+        }
+    }
+
+    public boolean startAR(View v)
+    {
+        final SampleData sampleData = categories.get(0).getSamples().get(0);
+        final String[] permissions = PermissionUtil.getPermissionsForArFeatures(sampleData.getArFeatures());
+
+        if(!sampleData.getIsDeviceSupporting()) {
+            showDeviceMissingFeatures(sampleData.getIsDeviceSupportingError());
+        } else {
+            permissionManager.checkPermissions(StartActivity.this, permissions, PermissionManager.WIKITUDE_PERMISSION_REQUEST, new PermissionManager.PermissionManagerCallback() {
+                @Override
+                public void permissionsGranted(int requestCode) {
+                    final Intent intent = new Intent(StartActivity.this, sampleData.getActivityClass());
+                    intent.putExtra(SimpleArActivity.INTENT_EXTRAS_KEY_SAMPLE, sampleData);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }
+
+                @Override
+                public void permissionsDenied(@NonNull String[] deniedPermissions) {
+                    Toast.makeText(StartActivity.this, getString(R.string.permissions_denied) + Arrays.toString(deniedPermissions), Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void showPermissionRationale(final int requestCode, @NonNull String[] strings) {
+                    final AlertDialog.Builder alertBuilder = new AlertDialog.Builder(StartActivity.this);
+                    alertBuilder.setCancelable(true);
+                    alertBuilder.setTitle(R.string.permission_rationale_title);
+                    alertBuilder.setMessage(getString(R.string.permission_rationale_text) + Arrays.toString(permissions));
+                    alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            permissionManager.positiveRationaleResult(requestCode, permissions);
+                        }
+                    });
+
+                    AlertDialog alert = alertBuilder.create();
+                    alert.show();
+                }
+            });
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        ArchitectView.getPermissionManager().onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    public void showDeviceMissingFeatures(String errorMessage) {
+        new AlertDialog.Builder(StartActivity.this)
+                .setTitle(R.string.device_missing_features)
+                .setMessage(errorMessage)
+                .show();
+    }
+}
